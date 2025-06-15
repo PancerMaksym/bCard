e55b4c87e7222ea33639cafb8d89cf46d9742c15
PancerMaksym <pantsyr.maksym@gmail.com>
2025-06-16 00:21:09 +0300

    Second commit

--- src/components/photoBlock.tsx
+++ src/components/photoBlock.tsx
@@ -3,19 +3,77 @@ import "./photoBlock.scss"
 import { StaticImage } from "gatsby-plugin-image"
 import { QRCodeSVG } from "qrcode.react"
 import { MdContentCopy } from "react-icons/md"
+import { Bounce, ToastContainer, toast } from "react-toastify"
 
 function PhotoBlock() {
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText("https://b-card-mu.vercel.app/")
+      .then(() => {
+        toast.success("Copied successfully!", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        })
+      })
+      .catch(() => {
+        toast.error("Failed to copy!", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        })
+      })
+  }
+
   return (
     <div className="photoBlock">
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+        transition={Bounce}
+      />
       <h1 className="name">Pantsyr Maksym</h1>
       <StaticImage alt={""} src="../images/remove.png" className="photo" />
       <div className="qrcode">
         <div className="button">
-          <MdContentCopy className="copy" />
+          <MdContentCopy className="copy" style={{ cursor: "pointer" }} onClick={handleCopy} />
         </div>
         <QRCodeSVG
           className="QRCodeSVG"
-          value="http://192.168.0.131:8000/"
+          value="https://b-card-mu.vercel.app/"
           height={100}
           width={100}
         />
